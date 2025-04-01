8ffd8f0bccca6455341f0360ad6b429fe394f6a2
Dopemon <dopemon.roa@gmail.com>
2025-03-31 21:05:55 -0700

    Added more Clairen Files

--- js/renderCharPage.js
+++ js/renderCharPage.js
@@ -8,10 +8,34 @@ $(`#home-container`).append(charMoveCard({
             nhb: "./files/videos/Clairen-nhb-Fstrong.webm?f=10"
         },
         {
-            name: "Bair",
-            presentationName:"Back Air",
-            whb: "./files/videos/Clairen-whb-Bair.webm?f=8",
-            nhb: "./files/videos/Clairen-nhb-Bair.webm?f=8"
+            name: "Ustrong",
+            presentationName:"Up Strong",
+            whb: "./files/videos/Clairen-whb-Ustrong.webm?f=11",
+            nhb: "./files/videos/Clairen-nhb-Ustrong.webm?f=11"
+        },
+        {
+            name: "Dstrong",
+            presentationName:"Down Strong",
+            whb: "./files/videos/Clairen-whb-Dstrong.webm?f=7",
+            nhb: "./files/videos/Clairen-nhb-Dstrong.webm?f=7"
+        },
+        {
+            name: "Ftilt",
+            presentationName:"Forward Tilt",
+            whb: "./files/videos/Clairen-whb-Ftilt.webm?f=7",
+            nhb: "./files/videos/Clairen-nhb-Ftilt.webm?f=7"
+        },
+        {
+            name: "Utilt",
+            presentationName:"Up Tilt",
+            whb: "./files/videos/Clairen-whb-Utilt.webm?f=8",
+            nhb: "./files/videos/Clairen-nhb-Utilt.webm?f=8"
+        },
+        {
+            name: "Dtilt",
+            presentationName:"Down Tilt",
+            whb: "./files/videos/Clairen-whb-Dtilt.webm?f=6",
+            nhb: "./files/videos/Clairen-nhb-Dtilt.webm?f=6"
         }
     ]
 }));
@@ -22,16 +46,28 @@ $(`#home-container`).append(charMoveCard({
     group: "Aerial Attacks",
     moves:[
         {
-            name: "Fstrong",
+            name: "Dair",
             presentationName:"Forward Strong",
-            whb: "./files/videos/Clairen-whb-Fstrong.webm?f=10",
-            nhb: "./files/videos/Clairen-nhb-Fstrong.webm?f=10"
+            whb: "./files/videos/Clairen-whb-Dair.webm?f=15",
+            nhb: "./files/videos/Clairen-nhb-Dair.webm?f=15"
         },
         {
             name: "Bair",
             presentationName:"Back Air",
             whb: "./files/videos/Clairen-whb-Bair.webm?f=8",
             nhb: "./files/videos/Clairen-nhb-Bair.webm?f=8"
+        },
+        {
+            name: "Nair",
+            presentationName:"Neutral Air",
+            whb: "./files/videos/Clairen-whb-Nair.webm?f=9",
+            nhb: "./files/videos/Clairen-nhb-Nair.webm?f=9"
+        },
+        {
+            name: "Uair",
+            presentationName:"Up Air",
+            whb: "./files/videos/Clairen-whb-Uair.webm?f=15",
+            nhb: "./files/videos/Clairen-nhb-Uair.webm?f=15"
         }
     ]
 }));
