ad9e7de4bc9271201909f483a8e8835fe3c6acb7
Dopemon <dopemon.roa@gmail.com>
2025-07-25 18:25:45 -0400

    Updated for wiki move-card

--- js/renderFrameViewer-wiki.js
+++ js/renderFrameViewer-wiki.js
@@ -20,7 +20,7 @@ $(window).load($(".fd-modal").toArray().forEach((modal, i) => {
     let iconPlay = (size) => $(`<svg xmlns="http://www.w3.org/2000/svg" width="${size}" height="${size} "fill="currentColor" class="bi bi-play-fill" viewBox="0 0 16 16"><path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393"/></svg>`);
     let iconPause = (size) => $(`<svg xmlns="http://www.w3.org/2000/svg" width="${size}" height="${size} "fill="currentColor" class="bi bi-pause-fill" viewBox="0 0 16 16"><path d="M5.5 3.5A1.5 1.5 0 0 1 7 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5m5 0A1.5 1.5 0 0 1 12 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5"/></svg>`);
     let iconInfo = (size) => $(`<svg xmlns="http://www.w3.org/2000/svg" width="${size}" height="${size} "fill="currentColor" class="bi bi-question-circle" viewBox="0 0 16 16"><path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/><path d="M5.255 5.786a.237.237 0 0 0 .241.247h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286m1.557 5.763c0 .533.425.927 1.01.927.609 0 1.028-.394 1.028-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94"/></svg>`);
-    let iconDownload = (size) => $(`<svg xmlns="http://www.w3.org/2000/svg" width="${size}" height="${size}" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16"><path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5"/><path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708z"/></svg>`)
+    // let iconDownload = (size) => $(`<svg xmlns="http://www.w3.org/2000/svg" width="${size}" height="${size}" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16"><path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5"/><path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708z"/></svg>`)
 
     let backwards = $( `<button class="fd-control-button move backward"></button>` ).append(iconBackwards(iconSize()));
     let forwards = $( `<button class="fd-control-button move forward"></button>` ).append(iconForwards(iconSize()));
@@ -33,12 +33,12 @@ $(window).load($(".fd-modal").toArray().forEach((modal, i) => {
     let pauseButton = $(`<button class="fd-control-button hide"></button>`).append(iconPause(iconSize()));
     let infoButton = $(`<button class="fd-control-button info"></button>`).append(iconInfo(iconSize()));
   
-    let downloadContainer = $(`<div class="download-container show-flex"></div>`)
+    // let downloadContainer = $(`<div class="download-container show-flex"></div>`)
     let infoContainer = $(`<div class="info-container"></div>`)
     let infoRow = $(`<div class="info-container-row"></div>`)
     let infoText = (text) => $(`<p font-size=1rem;>${text}</p>`);
   
-    downloadContainer.append(iconDownload(iconSize()));
+    // downloadContainer.append(iconDownload(iconSize()));
 
     infoContainer.append(infoRow.clone().append(iconPlay(iconSize(2))).append(infoText("Play video.")));
     infoContainer.append(infoRow.clone().append(iconPause(iconSize(2))).append(infoText("Pause video.")));
@@ -54,7 +54,8 @@ $(window).load($(".fd-modal").toArray().forEach((modal, i) => {
     video1.append(source1);
     video2.append(source2);
     infoContainer.append(infoRow.clone());
-    videoContainer.append(toggleIcons).append(video1).append(video2).append(infoContainer).append(controlsContainer).append(downloadContainer);
+    videoContainer.append(toggleIcons).append(video1).append(video2).append(infoContainer).append(controlsContainer)
+    // .append(downloadContainer);
     controlsContainer.append(backwards).append(playButton).append(pauseButton).append(forwards);
     modal.append(videoContainer);
   
@@ -92,14 +93,13 @@ $(window).load($(".fd-modal").toArray().forEach((modal, i) => {
     const fvHideElement = (showing) => {showing.removeClass("show").addClass("hide");}
     const fvShowElement = (hiding) => {hiding.removeClass("hide").addClass("show");}
     const fvToggleShow = (el) => { el.hasClass("show") ? fvHideElement(el) : fvShowElement(el) }
-        
-    [downloadContainer].forEach( el => el.on('click', () => { 
+    
+    const hitboxTab = modal.parent().parent().parent().parent().$("#tabber-Hitboxes-label");
+    [hitboxTab].forEach( el => el.on('click', () => { 
       video1[0].load();
       video2[0].load();
-      console.log(el);
-      el.removeClass("show-flex").addClass("hide");
-      console.log("told it to load")
     }));
+    // fdModal.parentElement.parentElement.parentElement.parentElement.querySelector("#tabber-Hitboxes-label")
 
     [video1, video2].forEach( el => el.on('canplaythrough', () => { 
         el[0].load();
